267446c0a05cb97b90eb1ccaae296f3f19f83abe
Prince-Lartey <lartey.princ@gmail.com>
2024-10-23 12:31:14 +0000

    ..

--- src/lib/firebase/firebase.js
+++ src/lib/firebase/firebase.js
@@ -15,7 +15,7 @@ const firebaseConfig = {
 	storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
 	messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
 	appId: import.meta.env.VITE_APP_ID,
-	measurementId: 'G-F5NQLB28V3'
+	measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
